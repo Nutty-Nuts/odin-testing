4848bef4a599b032925e2d7bc5e484a172b76099
Nutty-nuts <vegagerard1130@gmail.com>
2023-01-29 07:30:10 +0800

    optimized caesar cipher

--- caesarCipher.js
+++ caesarCipher.js
@@ -7,24 +7,36 @@ function caesarCipher(inputString, shiftAmount) {
 
     for (let item of characterString) {
         characterCode = item.charCodeAt() + shiftAmount;
-
-        if (item.charCodeAt() == 32) {
-            characterCode = 32;
-        } else if (item.charCodeAt() >= 65 && item.charCodeAt() <= 90) {
-            if (characterCode > 90) {
-                let moduluCode = characterCode % 90;
-                characterCode = 64 + moduluCode;
-            }
-        } else if (item.charCodeAt() >= 97 && item.charCodeAt() <= 122) {
-            if (characterCode > 122) {
-                let moduluCode = characterCode % 122;
-                characterCode = 96 + moduluCode;
-            }
-        } else {
-            characterCode -= shiftAmount;
-        }
+        characterCode = wrapLetters(
+            item.charCodeAt(),
+            characterCode,
+            shiftAmount
+        );
         outputString += String.fromCharCode(characterCode);
     }
     return outputString;
 }
+
+function wrapLetters(oldCharacterCode, newCharacterCode, shiftAmount) {
+    if (oldCharacterCode == 32) {
+        return 32;
+    } else if (oldCharacterCode >= 65 && oldCharacterCode <= 90) {
+        if (newCharacterCode > 90) {
+            let moduluCode = newCharacterCode % 90;
+            return 64 + moduluCode;
+        } else {
+            return newCharacterCode;
+        }
+    } else if (oldCharacterCode >= 97 && oldCharacterCode <= 122) {
+        if (newCharacterCode > 122) {
+            let moduluCode = newCharacterCode % 122;
+            return 96 + moduluCode;
+        } else {
+            return newCharacterCode;
+        }
+    } else {
+        return newCharacterCode - shiftAmount;
+    }
+}
+
 module.exports = caesarCipher;
